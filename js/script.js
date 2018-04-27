41e1c76d3762dd97670e64bdec14c6d7b420bb70
Jakub Kilar <kuba.kilar@gmail.com>
2018-04-27 21:13:56 +0200

    update

--- js/script.js
+++ js/script.js
@@ -2,8 +2,24 @@ let button = document.getElementById('addElem');
 
 button.addEventListener('click', function () {
   let list = document.getElementById('list');
-  let item = document.getElementsByTagName('li');
+  let items = document.getElementsByTagName('li');
   let newElem = document.createElement('li');
-  newElem.innerHTML = 'item ' + item.length;
+  newElem.innerHTML = 'item ' + items.length;
   list.appendChild(newElem);
 });
+
+// items bo tablica, czyli zawiera kilka elementów
+
+// var randomButtons = document.getElementsByClassName('button');
+// var buttonLenght = document.getElementsByClassName('button').length;
+// var buttonTxt = document.getElementsByTagName('button').innerText;
+
+// for (var i = 0 ; i < buttonLenght; i++) {
+//   alert('I will show up four times, because there are four button elements');
+
+// }
+
+
+// console.log(randomButtons);
+// console.log(buttonLenght);
+// console.log(buttonTxt);
